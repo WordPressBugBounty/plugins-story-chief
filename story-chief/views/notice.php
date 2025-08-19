d84b83b37c857c450d2b6d8b9a243ae54eb59250
Sean Murphy <sean@iamseanmurphy.com>
2025-08-19 21:18:06 +0000

    1.0.45

--- story-chief/views/notice.php
+++ story-chief/views/notice.php
@@ -1,48 +1,59 @@
 <?php if ( $type == 'parent-plugin' ) :?>
     <div id="storychief-warning" class="notice notice-warning is-dismissible">
         <p>
-            <strong><?php printf( esc_html__('StoryChief %s requires WP Rest API 2.0 or higher.', 'storychief'), STORYCHIEF_VERSION);?></strong>
-            <?php printf(__('Please <a href="%1$s">install WordPress REST API</a>.', 'storychief'), 'https://wordpress.org/plugins/rest-api/');?>
+            <strong>
+                <?php
+                /* translators: Plugin version number */
+                printf( esc_html__('StoryChief %s requires WP Rest API 2.0 or higher.', 'story-chief'), esc_html(STORYCHIEF_VERSION));
+                ?>
+            </strong>
+            <?php printf(esc_html__('Please install WordPress REST API.', 'story-chief')); ?>
+            <a href="https://wordpress.org/plugins/rest-api/"><?php printf(esc_html__('Download', 'story-chief')); ?></a>
         </p>
     </div>
 <?php elseif ( $type == 'wpml-plugin' ) :?>
     <div id="storychief-warning" class="notice notice-info is-dismissible">
         <p>
-            <strong><?php printf( esc_html__('There is an extension available for WPML', 'storychief'));?></strong>
-            <?php printf(__('Download it <a href="%1$s">here</a>.', 'storychief'), 'https://wordpress.org/plugins/story-chief-wpml/');?>
+            <strong><?php printf( esc_html__('There is an extension available for WPML', 'story-chief'));?></strong>
+            <a href="https://wordpress.org/plugins/story-chief-wpml/"><?php printf(esc_html__('Download', 'story-chief')); ?></a>
         </p>
     </div>
 <?php elseif ( $type == 'polylang-plugin' ) :?>
     <div id="storychief-warning" class="notice notice-info is-dismissible">
         <p>
-            <strong><?php printf( esc_html__('There is an extension available for Polylang', 'storychief'));?></strong>
-            <?php printf(__('Download it <a href="%1$s">here</a>.', 'storychief'), 'https://wordpress.org/plugins/story-chief-polylang/');?>
+            <strong><?php printf( esc_html__('There is an extension available for Polylang', 'story-chief'));?></strong>
+            <a href="https://wordpress.org/plugins/story-chief-polylang/"><?php printf(esc_html__('Download', 'story-chief')); ?></a>
         </p>
     </div>
 <?php elseif ( $type == 'acf-plugin' ) :?>
     <div id="storychief-warning" class="notice notice-info is-dismissible">
         <p>
-            <strong><?php printf( esc_html__('There is an extension available for ACF', 'storychief'));?></strong>
-            <?php printf(__('Download it <a href="%1$s">here</a>.', 'storychief'), 'https://wordpress.org/plugins/storychief-acf/');?>
+            <strong><?php printf( esc_html__('There is an extension available for ACF', 'story-chief'));?></strong>
+            <a href="https://wordpress.org/plugins/storychief-acf/"><?php printf(esc_html__('Download', 'story-chief')); ?></a>
         </p>
     </div>
 <?php elseif ( $type == 'version' ) :?>
     <div id="storychief-warning" class="notice notice-warning is-dismissible">
         <p>
-            <strong><?php printf( esc_html__('StoryChief %s requires WordPress 4.6 or higher.', 'storychief'), STORYCHIEF_VERSION);?></strong>
-            <?php printf(__('Please <a href="%1$s">upgrade WordPress</a> to a current version.', 'storychief'), 'http://codex.wordpress.org/Upgrading_WordPress');?>
+            <strong>
+                <?php
+                /* translators: Plugin version */
+                printf( esc_html__('StoryChief %s requires WordPress 4.6 or higher.', 'story-chief'), esc_html(STORYCHIEF_VERSION));
+                ?>
+            </strong>
+            <a href="http://codex.wordpress.org/Upgrading_WordPress"><?php printf(esc_html__('Upgrade', 'story-chief')); ?></a>
         </p>
     </div>
 <?php elseif( $type == 'config-set') : ?>
     <div id="storychief-warning" class="notice notice-success is-dismissible">
         <p>
-            <strong><?php printf( esc_html__('Configuration saved', 'storychief'));?></strong>
+            <strong><?php printf( esc_html__('Configuration saved', 'story-chief'));?></strong>
         </p>
     </div>
 <?php elseif( $type == 'undefined') : ?>
     <div id="storychief-warning" class="notice notice-error is-dismissible">
         <p>
-            <strong><?php printf( esc_html__('An unknown error occurred', 'storychief'));?></strong>
+            <strong><?php printf( esc_html__('An unknown error occurred', 'story-chief'));?></strong>
         </p>
     </div>
 <?php endif; ?>
